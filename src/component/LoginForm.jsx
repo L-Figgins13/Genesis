ba79e27ad2fe8cbf011377f8a1b8d9a83a336f0e
Logan Figgins <l.figgins13@gmail.com>
2018-04-24 20:28:34 -0700

    testing out styled.components

--- src/component/LoginForm.jsx
+++ src/component/LoginForm.jsx
@@ -1,25 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+
+const Label = styled.label`
+    color: blue;
+`;
+const Input = styled.input`
+    color: purple;
+    background: papayawhip;
+    border-radius: 3px;
+`;
+
+const Button = styled.button`
+    color:gold;
+`
+
+
+
+// const LoginForm = function (props) {
+//     return (
+//         <div>
+//             <form onSubmit= {props.handleSubmit}>
+//                 <label>
+//                     Username:
+//                     <input name = "username" type = "text" value = {props.username} onChange= {props.handleInputChange} />
+//                 </label>
+//                 <br />
+//                 <label>
+//                     password:
+//                     <input name="password" type="password" value={props.password} onChange= {props.handleInputChange} />
+//                 </label>
+//                 <input type = "submit" value = "Login"/>
+//             </form>
+//             <ul>
+//                 <li><Link to= '/games/create'>Create a Game</Link></li>
+//             </ul>
+//         </div>
+//     )
+// }
 
 const LoginForm = function (props) {
     return (
         <div>
             <form onSubmit= {props.handleSubmit}>
-                <label>
-                    Username:
-                    <input name = "username" type = "text" value = {props.username} onChange= {props.handleInputChange} />
-                </label>
-                <br />
-                <label>
-                    password:
-                    <input name="password" type="password" value={props.password} onChange= {props.handleInputChange} />
-                </label>
-                <input type = "submit" value = "Login"/>
+                <Label>Username</Label>
+                <Input name = "username" type="text" value = {props.username} onChange= {props.handleInputChange}  />
+                <Label>Password</Label>
+                <Input name = "password" type="text" value={props.password} onChange= {props.handleInputChange} />
+                <Button type = "submit">Submit </Button>
             </form>
-            <ul>
-                <li><Link to= '/games/create'>Create a Game</Link></li>
-            </ul>
         </div>
     )
 }
