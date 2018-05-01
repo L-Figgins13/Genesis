42843a6fd5a8f7bf4923beca1390416288ebf493
Logan Figgins <l.figgins13@gmail.com>
2018-05-01 14:17:56 -0700

    begin styling for login form

--- src/blocks/LoginForm/index.jsx
+++ src/blocks/LoginForm/index.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
 import { Flex, Box } from 'grid-styled';
+import backgroundImg from '../../../static/img/backgrounds/bg1.jpg';
+import plateImg from '../../../static/img/login/login-plate.png';
 
 const Label = styled.label`
     color: blue;
@@ -10,24 +12,65 @@ const Label = styled.label`
 const Input = styled.input`
     color: purple;
     background: papayawhip;
-    border-radius: 3px;
 `;
 
 const Button = styled.button`
     color:gold;
 `
 
+const Bar = styled.div`
+    border: 1px solid red;
+`
+
+const Background = styled.div`
+    background-image: url(${backgroundImg});
+    background-size: cover;
+    background-attachment: fixed;
+    background-position: center center;
+    background-repeat: no-repeat;
+    overflow: hidden;
+    height: 100%;
+    width: 100vw;
+    margin: 0;
+    padding:0;
+`
+
+const LoginPlate = styled.div`
+    height: 60vh;
+    min-height: 75vh;
+    border : solid 1px rgba(0,0,0,0.0);
+    background-image: url(${plateImg});
+    background-repeat: no-repeat;
+    background-position: 50% 50%;
+    background-size: 920px 685px;
+    max-height:685px;
+`;
+
 const LoginForm = function (props) {
     return (
-        <div>
-            <form onSubmit= {props.handleSubmit}>
-                <Label>Username</Label>
-                <Input name = "username" type="text" value = {props.username} onChange= {props.handleInputChange}  />
-                <Label>Password</Label>
-                <Input name = "password" type="text" value={props.password} onChange= {props.handleInputChange} />
-                <Button type = "submit">Submit </Button>
-            </form>
-        </div>
+        
+        <Flex mx={0} my={0}>
+            <Background>
+                <Box>
+                    <LoginPlate>
+                        <form onSubmit= {props.handleSubmit}>
+                            <Box>
+                                <Bar />
+                                <Label>Username</Label>
+                                <Input name = "username" type="text" value = {props.username} onChange= {props.handleInputChange}  />
+                            </Box>
+                            <Box>
+                                <Bar />
+                                <Label>Password</Label>
+                                <Input name = "password" type="text" value={props.password} onChange= {props.handleInputChange} />
+                            </Box>
+                            <Button type = "submit">Submit </Button>
+                        </form>
+                    </LoginPlate>
+                </Box>
+            </Background>
+        </Flex>
+        
     )
 }
 
