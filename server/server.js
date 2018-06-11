46735259e279749cc1ffb68c9e141f5c4fbba319
Logan Figgins <l.figgins13@gmail.com>
2018-06-11 15:46:26 -0700

    switching branches

--- server/server.js
+++ server/server.js
@@ -11,7 +11,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import SourceMapSupport from 'source-map-support';
 
-
 import authCheck from './auth-check.js';
 import api from './routes/api.js';
 import auth from './routes/auth.js';
